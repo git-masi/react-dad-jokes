41638c2b67c5e5601c1569406820096055326867
Eric Masi <eric@gitmasi.com>
2019-04-29 16:46:44 -0400

    get one joke from API

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import './JokeList.css';
+import axios from 'axios';
 import Joke from './Joke';
 
 class JokeList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokeURL: null
+    }
+  }
+
+  async componentDidMount() {
+  let res = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: "application/json"}});
+    console.log(res.data.joke);
+  }
+
   render() {
     return (
       <Joke />
