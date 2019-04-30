e58fd26a17f9bee2bdbb0d36d05b7ad11103b499
Eric Masi <eric@gitmasi.com>
2019-04-30 13:23:59 -0400

    conditionally change vote color display and emoji

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -16,16 +16,53 @@ class Joke extends Component {
     this.props.click(this.props.id, -1);
   }
 
+  getColor() {
+    const votes = this.props.votes;
+    switch (true) {
+      case votes >= 15 :
+        return "#4CAF50";
+      case votes >= 12 :
+        return "#8BC34A";
+      case votes >= 9 :
+        return "#CDDC39";
+      case votes >= 6 :
+        return "#FFEB3B";
+      case votes >= 3 :
+        return "#FFC107";
+      case votes >= 0 :
+        return "#FF9800";
+      default:
+        return "#f44336";
+    }
+  }
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (this.props.votes >= 12) {
+      return "em em-laughing";
+    } else if (this.props.votes >= 9) {
+      return "em em-smiley";
+    } else if (this.props.votes >= 6) {
+      return "em em-slightly_smiling_face";
+    } else if (this.props.votes >= 3) {
+      return "em em-neutral_face";
+    } else if (this.props.votes >= 0) {
+      return "em em-confused";
+    } else {
+      return "em em-angry";
+    }
+  }
+
   render() {
     return (
       <div className="Joke">
         <div className="Joke-vote-container">
           <i onClick={this.voteUp} className="far fa-thumbs-up"></i>
-          <span className="Joke-votes">{this.props.votes}</span>
+          <span className="Joke-votes" style={{borderColor: this.getColor()}}>{this.props.votes}</span>
           <i onClick={this.voteDown} className="far fa-thumbs-down"></i>
         </div>
         <div className="Joke-text">{this.props.text}</div>
-        {/* <div>emoji goes here</div> */}
+        <i className={this.getEmoji()}></i>
       </div>
     )
   }
