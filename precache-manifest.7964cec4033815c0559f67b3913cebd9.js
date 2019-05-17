54e65045fc9655cc7621f1dddec1e4d1fb385333
Eric Masi <eric@gitmasi.com>
2019-05-17 14:52:05 -0400

    Updates

---
+++ precache-manifest.7964cec4033815c0559f67b3913cebd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6ee286143563d583a55ebaa038b3f1",
+    "url": "/react-dad-jokes/index.html"
+  },
+  {
+    "revision": "26dbdd23e99d810b8622",
+    "url": "/react-dad-jokes/static/css/main.111ec4cd.chunk.css"
+  },
+  {
+    "revision": "733132cd17b62e708df7",
+    "url": "/react-dad-jokes/static/js/2.ae18236f.chunk.js"
+  },
+  {
+    "revision": "26dbdd23e99d810b8622",
+    "url": "/react-dad-jokes/static/js/main.1a244090.chunk.js"
+  },
+  {
+    "revision": "860a967136e351ef7c01",
+    "url": "/react-dad-jokes/static/js/runtime~main.a58e043a.js"
+  },
+  {
+    "revision": "6fd227e4b6dc0704d760f5e893c2149e",
+    "url": "/react-dad-jokes/static/media/crying-laughing-emoji.6fd227e4.svg"
+  }
+]);
